fc84cd6d844c864e2e2d2ef1e3c2890ea8215c5d
AmitZur <amit@xplg.com>
2024-02-20 11:31:23 +0200

    Initial commit

---
+++ app/src/main/java/com/example/assignment_recyclerview/Models.java
@@ -0,0 +1,33 @@
+package com.example.assignment_recyclerview;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Models {
+    static String[] names ={"Jon Snow","Eddard Stark","Robb Stark","Sansa Stark","Arya Stark","Tyrion Lannister","Cersei Lannister","Daenerys Targaryen","Ramsay Bolton","White Walker"};
+    static String[] description = {"the noble bastard of Winterfell, embodies a resilient and honorable leader in the tumultuous realm of Westeros, marked by his unwavering commitment to justice and duty",
+            "the honorable Lord of Winterfell , epitomizes loyalty, integrity, and a steadfast commitment to honor in the face of treacherous political intrigue",
+            "Robb Stark, the charismatic and honorable young , commands loyalty and respect as he strives to uphold his family's honor in the midst of war and political turmoil",
+            "Sansa Stark navigates a harrowing journey from innocence to cunning, showcasing her strength and adaptability amid the turbulent political landscape of Westeros.",
+            "Arya Stark, the fiercely independent and skilled assassin, embodies determination and resilience as she transforms from a young girl seeking justice into a formidable force defying societal expectations",
+            "Tyrion Lannister, the witty and cunning dwarf  combines sharp intelligence with a unique sense of humor, navigating the treacherous political intrigues of Westeros with a keen strategic mind",
+            "Cersei Lannister, the cunning and ruthless queen, manipulates power with a fierce determination, often blurring the lines between ambition and amorality in her pursuit of the Iron Throne",
+            "Daenerys Targaryen, the last surviving member of her house , evolves from a vulnerable exile into a formidable leader with a sense of justice, fueled by her unyielding determination to reclaim the Iron Throne and break the wheel of power in Westeros",
+            "Ramsay Bolton, the sadistic and ruthless antagonist, inflicts terror with his cruelty and cunning, epitomizing the dark and chaotic underbelly of Westeros",
+            "The White Walkers, ethereal and otherworldly beings, instill fear with their icy presence and formidable army of the dead, representing an existential threat to the realm of Westeros"};
+    static String[] extraDescOnKingdoms = {
+            "Jon Snow is the bastard sun of Eddard Stark and he lives in Winterfell is the capital of the Kingdom of the North and the seat and the ancestral home of the royal House Stark",
+            "Eddard Stark is the king of Winterfell is the capital of the Kingdom of the North and the seat and the ancestral home of the royal House Stark",
+            "Robb Stark the son of Eddard Stark, he becomes the King of the North after his father dies, his kingdom is Winterfell is the capital of the Kingdom of the North and the seat and the ancestral home of the royal House Stark",
+            "Sansa Stark is the daughter of Eddard Stark, she becomes the Lady of Winterfell after her mother dies, her kingdom is Winterfell is the capital of the Kingdom of the North and the seat and the ancestral home of the royal House Stark",
+            "Arya Stark is the daughter of Eddard Stark, she becomes the Lady of Winterfell after her mother dies, her kingdom is Winterfell is the capital of the Kingdom of the North and the seat and the ancestral home of the royal House Stark",
+            "Tyrion Lannister is the youngest child of Lord Tywin Lannister and younger brother of Cersei and Jaime Lannister",
+            "Queen Cersei Lannister was the twentieth ruler of the Seven Kingdoms and the widow of King Robert Baratheon",
+            "Queen Daenerys Targaryen, also known as Daenerys Stormborn, and affectionately known as Dany, was the only daughter of King Aerys II Targaryen and Queen Rhaella",
+            "Lord Ramsay Bolton, born Ramsay Snow, was the legitimized bastard son of Roose Bolton, Lord of the Dreadfort, and the unidentified wife of a miller",
+            "The White Walkers were an ancient race of formerly-human ice creatures who came from the far north of Westeros"
+    };
+    static int[] _id = {R.drawable.jon_snow,R.drawable.eddard_stark,R.drawable.robb_stark,R.drawable.sansa,
+            R.drawable.arya_stark,R.drawable.tyrion_lannister,R.drawable.cersei_lannister,R.drawable.daenerys,R.drawable.ramsay,
+            R.drawable.white_walkers};
+}
